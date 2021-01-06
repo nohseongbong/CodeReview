d2e825d10559a919a5395d6f21f9ebdafc3e3656
youngyoung0 <kxodud10054@gmail.com>
2021-01-06 17:00:12 +0900

    Create Student.java

---
+++ youngyoung/210107/Student.java
@@ -0,0 +1,72 @@
+package studentClass;
+
+public class Student {
+	private String classNo;
+	private String name;
+	private int age;
+	private int korScore;
+	private int engScore;
+	private int mathScore;
+	
+	public Student(String classNo, String name, int age, int korScore, int engScore, int mathScore) {
+		this.classNo = classNo;
+		this.name = name;
+		this.age = age;
+		this.korScore = korScore;
+		this.engScore = engScore;
+		this.mathScore = mathScore;
+	}
+
+
+
+
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getKorScore() {
+		return korScore;
+	}
+	public void setKorScore(int korScore) {
+		this.korScore = korScore;
+	}
+	public int getEngScore() {
+		return engScore;
+	}
+	public void setEngScore(int engScore) {
+		this.engScore = engScore;
+	}
+	public int getMathScore() {
+		return mathScore;
+	}
+	public void setMathScore(int mathScore) {
+		this.mathScore = mathScore;
+	}
+
+	public String getClassNo() {
+		return classNo;
+	}
+
+	public void setClassNo(String classNo) {
+		this.classNo = classNo;
+	}
+	
+	
+}
