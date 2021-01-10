30a0277db2ecbc0fd4e5e1c526e586e35094f86e
youngyoung0 <kxodud10054@gmail.com>
2021-01-10 23:47:42 +0900

    누락된 설명이 있어서 추가하여서 다시 재업로드하였습니다.

--- homework/video/src/controller/VideoController.java
+++ homework/video/src/controller/VideoController.java
@@ -1,5 +1,9 @@
 package controller;
 
+// controller
+// view에서 처리해야 할 데이터를 받아온다.
+// 담당할 service를 선택하여 호출한다.
+
 public class VideoController {
 
 }
--- homework/video/src/service/VideoService.java
+++ homework/video/src/service/VideoService.java
@@ -1,5 +1,8 @@
 package service;
 
+// service
+// DAO를 호출하여 데이터를 받아 비즈니스 로직을 처리합니다.
+
 public class VideoService {
 
 }
